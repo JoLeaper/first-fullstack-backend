d7dceb70b62c338077667f4276486d0469001465
JoLeaper <joleap123@gmail.com>
2020-05-10 16:45:11 -0700

    most recent attempt at bugfixing

--- server.js
+++ server.js
@@ -13,19 +13,20 @@ app.get('/digimon', async(req, res) => {
 });
 
 app.get('/digimon/:id', async(req, res) => {
-  const name = req.params.name;
-  const result = await client.query('SELECT * from digimon WHERE name = $1',
-    [name]);
+  const id = req.params.id;
+  const result = await client.query('SELECT * FROM digimon WHERE digimon_name = $1',
+    [id]);
   res.json(result.rows[0]);
 });
 
-app.get('/digimon/', async(req, res) => {
+app.post('/digimon/', async(req, res) => {
+  console.log(req.body);
   const data = await client.query(`
                                   INSERT INTO digimon (digimon_name, digimon_level, digimon_type, digimon_attribute, digimon_attack, appeared_in_anime)
                                   VALUES ($1, $2, $3, $4, $5, $6)
                                   RETURNING *;
               `,
-  (req.body.digimon_name, req.body.digimon_level, req.body.digimon_type, req.body.digimon_attribute, req.body.digimon_attack, req.body.appeared_in_anime)
+  [req.body.digimon_name, req.body.digimon_level, req.body.digimon_type, req.body.digimon_attribute, req.body.digimon_attack, req.body.appeared_in_anime]
   );
   res.json(data.rows[0]);
 });
